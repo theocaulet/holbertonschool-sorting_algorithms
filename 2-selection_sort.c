32645853b67a1b1ce269ebbedce3bbe849d35353
Florian <12160@holbertonstudents.com>
2025-12-11 15:12:50 +0100

    Move print_array call outside inner loop in selection_sort for improved efficiency

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -28,8 +28,8 @@ void selection_sort(int *array, size_t size)
 				tmp = array[i];
 				array[i] = array[minj];
 				array[minj] = tmp;
-				print_array(array, size);
 			}
 		}
+		print_array(array, size);
 	}
 }
